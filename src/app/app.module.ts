5260d53e6199863adb18727d861b3edca3cd8e95
makapx <parlavecchiom98@gmail.com>
2023-09-20 19:12:31 +0200

    chore: add eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Input, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
