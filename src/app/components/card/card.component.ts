6243607b9b7b34f91f075390193c1dc0e5b868c0
makapx <parlavecchiom98@gmail.com>
2023-09-24 10:09:50 +0200

    refactor(cardcomponent): add changeDetection to decorator

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import Pokemon from '../../models/Pokemon';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
   @Input() pokemon: Pokemon|null = null;
