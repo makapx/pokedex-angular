c591ae6f282edd8270c18747a88c521f4f57e662
makapx <parlavecchiom98@gmail.com>
2023-09-24 14:31:16 +0200

    feat(searchbarcomponent,containercomponent): show search result in container component

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,42 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
 import Pokemon from '../../models/Pokemon';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.css']
+  styleUrls: ['./searchbar.component.css'],
 })
 /**
  * @class SearchbarComponent
- * @description This component is responsible for the searchbar
+ * @description This Outputcomponent is responsible for the searchbar
  * @property { string } pokemonName - The name of the pokemon to search for
- * 
- * @method ngOnInit - This method is responsible for initializing the component
+ *
  * @method searchPokemon - This method is responsible for searching for the pokemon
- * 
+ *
  * @example <app-searchbar></app-searchbar>
- * 
- * @returns { void } 
+ *
+ * @returns { void }
  */
 export class SearchbarComponent {
   pokemonName!: string;
-  pokemonInfo : Observable<Pokemon> = new Observable<Pokemon>();
-  
+
+  @Output()
+  readonly searchedPokemonInfo = new EventEmitter<Observable<
+    Pokemon[]
+  > | null>();
+
   // Inject the pokemon service into the constructor
-  constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService) {}
 
   /**
    * @method searchPokemon
    * @description This method is responsible for searching for the pokemon.
-   * If the pokemonName is not empty, it will return the pokemon with the given name.
-   * 
+   * If the pokemonName is not empty, it will invoke the getPokemon method from the pokemon service.
+   *
    * @param { string } pokemonName - The name of the pokemon to search for
-   * 
+   *
    * @returns { void }
    */
-  searchPokemon(pokemonName: string) {
-    if (pokemonName === '') return;
-    this.pokemonInfo = this.pokemonService.getPokemon(pokemonName);
+  searchPokemon(pokemonName: string): void {
+    if (!pokemonName) {
+      this.searchedPokemonInfo.emit(this.pokemonService.getPokemons());
+      return;
+    }
+
+    this.searchedPokemonInfo.emit(
+      this.pokemonService
+        .getPokemon(pokemonName)
+        .pipe(map((pokemon) => [pokemon]))
+    );
   }
 }
